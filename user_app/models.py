5d131be13c66d3f1302e84f8339bbb6bd8d3c95d
veroneoriginal <veronikanizovaya@gmail.com>
2024-06-24 19:34:04 +0300

    add admin profile

--- user_app/models.py
+++ user_app/models.py
@@ -1,17 +1,14 @@
-from django.core.validators import EmailValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    # класс от которого наследуемся имеет username, first_name, last_name, email, password
-
-    username = models.CharField(unique=False, null=False)
+    username = models.CharField(unique=False, null=False, max_length=150)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
-        return self.username
+        return self.email
