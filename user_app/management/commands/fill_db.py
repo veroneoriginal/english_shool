0048677a954b18926874cc2ad509bcc4556c1cd8
veroneoriginal <veronikanizovaya@gmail.com>
2024-06-24 21:47:47 +0300

    models have been created

--- user_app/management/commands/fill_db.py
+++ user_app/management/commands/fill_db.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
-
+from django.contrib.auth import get_user_model
 from user_app.models import User
 
 
@@ -11,17 +11,29 @@ class Command(BaseCommand):
         fake = Faker('ru-ru')
         count_people_for_db = 20
 
+        print('Удаляю всех пользователей, кроме админа ...')
+        User.objects.filter(is_superuser=False).delete()
+
+        print('Create users ... ')
+        roles = ['registered', 'student', 'teacher']
+
         for people in range(count_people_for_db):
             username = fake.user_name()
             email = fake.email()
             phone_number = fake.phone_number()[:15]
+            role = roles[0]
+            if people < 3:
+                role = roles[2]
+            elif people < 13:
+                role = roles[1]
+
             User.objects.create_user(
                 username=username,
                 email=email,
                 phone_number=phone_number,
-                password='qwerty123'
+                password='qwerty123',
+                role=role
             )
 
-        self.stdout.write(
-            self.style.SUCCESS('Success')
-        )
+        self.stdout.write(self.style.SUCCESS('База данных успешно заполнена пользователями и ролями'))
+
--- user_app/models.py
+++ user_app/models.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('registered', 'Registered'),
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+    ]
+
     username = models.CharField(unique=False, null=False, max_length=150)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='registered')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
