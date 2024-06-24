111e5a8377a960c643b04cf741350fb705653978
veroneoriginal <veronikanizovaya@gmail.com>
2024-06-24 22:22:25 +0300

    made changes to the principle of creating models

--- user_app/management/commands/fill_db.py
+++ user_app/management/commands/fill_db.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
-from django.contrib.auth import get_user_model
-from user_app.models import User
+from user_app.models import User, Role
 
 
 class Command(BaseCommand):
@@ -14,26 +13,33 @@ class Command(BaseCommand):
         print('Удаляю всех пользователей, кроме админа ...')
         User.objects.filter(is_superuser=False).delete()
 
-        print('Create users ... ')
+        print('Create roles ...')
         roles = ['registered', 'student', 'teacher']
+        role_objects = {}
+        for role in roles:
+            role_objects[role], created = Role.objects.get_or_create(name=role)
 
+        print('Create users ... ')
         for people in range(count_people_for_db):
             username = fake.user_name()
             email = fake.email()
             phone_number = fake.phone_number()[:15]
-            role = roles[0]
-            if people < 3:
-                role = roles[2]
-            elif people < 13:
-                role = roles[1]
 
-            User.objects.create_user(
+            user = User.objects.create_user(
                 username=username,
                 email=email,
                 phone_number=phone_number,
                 password='qwerty123',
-                role=role
             )
+
+            # Назначение ролей пользователям
+            if people < 3:
+                user.role.add(role_objects['teacher'])
+            if people < 13:
+                user.role.add(role_objects['student'])
+            if people >= 13:
+                user.role.add(role_objects['registered'])
+            user.save()
 
         self.stdout.write(self.style.SUCCESS('База данных успешно заполнена пользователями и ролями'))
 
--- user_app/models.py
+++ user_app/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-    ROLE_CHOICES = [
-        ('registered', 'Registered'),
-        ('student', 'Student'),
-        ('teacher', 'Teacher'),
-    ]
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
     username = models.CharField(unique=False, null=False, max_length=150)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(unique=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='registered')
+    role = models.ManyToManyField(Role, related_name='users')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
