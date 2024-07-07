511d9b3b8dce058c907e35167b950e5b7997f1a7
veroneoriginal <veronikanizovaya@gmail.com>
2024-07-07 18:42:28 +0300

    page with contacts is done

---
+++ main_app/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'class': 'form-control'}
+        )
+    )
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,24 @@
 from django.views.generic import (
     TemplateView,
+    FormView
 )
+from .forms import ContactForm
+from django.urls import reverse_lazy
 
 
 class IndexView(TemplateView):
     """Представление главной страницы"""
 
     template_name = 'main_app/index.html'
+
+
+class ContactView(FormView):
+    """Представление страницы с контактами"""
+    form_class = ContactForm
+    success_url = reverse_lazy('main_app:index')
+    template_name = 'main_app/contacts.html'
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        # print('MESSAGE', data['message'])
+        return super().form_valid(form)
